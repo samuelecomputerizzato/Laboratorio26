55c4316851874c2a5893f4134d1eba9d1a81a8a4
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-10 17:43:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color:#bfa76f;
+       
         background: linear-gradient(to bottom, :#bfa76f 50%, #6E8B3D 50%);
         color: #000000;
     }
