f284d80072e592df5abd0e112e54f11a92f9a299
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-10 18:16:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,7 @@ st.markdown(
 st.header("La Magna via")
 
 # Gestione dinamica del percorso dell'immagine
-if os.path.exists("RAG Samuele/Chatbot.webp"):
-    st.image("Chatbot.webp", width=500)
+st.image("Chatbot.webp", width=500)
 
 
 # FIX PERCORSO FILE: Trova il PDF nella stessa cartella di questo file app.py
