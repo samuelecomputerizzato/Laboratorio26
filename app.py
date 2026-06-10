1a9e8efede0cade0a29a40e48eed5c1e9573b661
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-10 18:21:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ st.markdown(
 st.header("La Magna via")
 
 # Gestione dinamica del percorso dell'immagine
-st.image("MAGNAVIA.WEBP", width=1000)
+st.image("MAGNAVIA.webp", width=500)
 
 
 # FIX PERCORSO FILE: Trova il PDF nella stessa cartella di questo file app.py
