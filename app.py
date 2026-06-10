862c1cbb7ab9eba70ec6c19dcbafa99e4da57da0
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-10 19:03:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ st.markdown(
 st.header("La Magna via")
 
 # Gestione dinamica del percorso dell'immagine
-st.image("SfondoChatbot.webp", width=1000)
+st.image("SfondoChatbot.webp", width=500)
 
 
 # FIX PERCORSO FILE: Trova il PDF nella stessa cartella di questo file app.py
