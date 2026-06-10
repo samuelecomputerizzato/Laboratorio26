912f61a67a83aa201842970660e8506549c7fa4b
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-10 20:05:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.markdown(
     .stApp {
         background-color:#B5A585;
         background-attachment: fixed;
-        color: #000000;
+        color: #4F1D0F;
         font-size: 36px;
     }
     
