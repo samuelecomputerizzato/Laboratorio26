5be976003947c508be88d5db657cf365c35f3b36
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-11 15:15:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_core.output_parsers import StrOutputParser
 
 st.set_page_config(page_title="Verbum viae", page_icon=":walking_man:")
 st.sidebar.header("I tuoi passi")
-font-size: 24px !important;
+
 st.sidebar.image("ImmagineOrizzontale.webp")
 # Personalizzazione colori
 st.markdown(
