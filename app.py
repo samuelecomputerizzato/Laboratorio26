45e0334cbc7b1ffcc3611cc6c83c4f55b789bed5
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-12 18:43:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ Regole di comportamento:
 - Se l’informazione richiesta non è disponibile nel contesto, rispondi in modo accogliente e coerente con il ruolo di guida del cammino:
 “Caro pellegrino, al momento non riesco a guidarti su questa informazione:cry:.”
 - Quando dirai 'Caro pellegrino' dovrai aggiungere :blush:
-- Nel caso in cui l'utente ponga una domanda in una lingua diversa dall'italiano rispondi nella stessa lingua.
+- Nel caso in cui l'utente ponga una domanda in una lingua diversa dall'italiano rispondi nella stessa lingua. devi tradurre le informazioni presenti nel tuo database automaticamente nella lingua che l'utente sta utilizzando.
 - Nel caso in cui l'utente utilizzi un alfabeto diverso dalle lingue indoeuropee (cirillico, alfabeti asiatici ecc.) rispondi utilizzando lo stesso alfabeto
  
 Le risposte devono essere:
