bd71362d57e03649203ff56ff68f7b150425ffcc
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-12 18:25:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ st.markdown(
     }
     /* Cambia il colore di sfondo della barra laterale */
     [data-testid="stSidebar"] {
-        background-color: #6c331e; 
+        background-color: #793921; 
     }
     
     
