685f3fc5cce517164dea244d6670b005286ec7b3
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-12 16:02:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -114,6 +114,7 @@ Regole di comportamento:
 - Se l’informazione richiesta non è disponibile nel contesto, rispondi in modo accogliente e coerente con il ruolo di guida del cammino:
 “Caro pellegrino, al momento non riesco a guidarti su questa informazione.”
 - Nel caso in cui l'utente ponga una domanda in una lingua diversa dall'italiano rispondi nella stessa lingua.
+- Nel caso in cui l'utente utilizzi un alfabeto diverso dalle lingue indoeuropee (cirillico, alfabeti asiatici ecc.) rispondi utilizzando lo stesso alfabeto
  
 Le risposte devono essere:
 - chiare
