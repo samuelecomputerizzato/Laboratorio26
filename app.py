9b955827589ad8095486f3920d845c292480899c
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-16 16:31:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ st.markdown("<p style='text-align: center; color: #542E17; font-weight: bold; fo
 # Elaborazione Documento PDF e RAG
 # -------------------------------------------------------------------
 cartella_corrente = os.path.dirname(__file__)
-documento = os.path.join(cartella_corrente, "pdf definitivo.pdf")
+documento = os.path.join(cartella_corrente, "pdf finale.pdf")
 catena = None
 
 if os.path.exists(documento):
