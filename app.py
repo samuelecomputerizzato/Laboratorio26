68f46c12d2ad11c1b8567e22048a64ccff2d15a0
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-18 10:14:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ st.header("La Magna via")
 # -------------------------------------------------------------------
 
 # Identifica il percorso del documento PDF
-documento = "Pdf finale (1)"
+documento = "Pdf finale (1).pdf"
 
 # Estrazione e spezzettamento del contenuto 
 if documento is not None:
