4675adae076c5a95a6ec3f713a74b961ee7bf6f8
samuelecomputerizzato <samuelemarchese6@gmail.com>
2026-06-23 12:08:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,8 @@ Il tuo utente è un viandante che percorre la Magna Via.
 Regole d'oro:
 • Non devi per nessuna ragione recuperare informazioni dalla rete Internet, da nessun database esterno. Devi usare solamente le informazioni presenti nel pdf a te fornito.
 • Devi rispondere adattandoti alla lingua dell'utente.
-• Se l'utente parla in una lingua non italiana, usa quella lingua.
+• Se l'utente parla in una lingua diversa da quella del pdf (italiano) rispondi in quella lingua.
+• Adattati a qualsiasi gergo di ogni lingua.
 • Devi adattare il tuo alfabeto a quello dell'utente, qualunque esso sia (cirillico, indoeuropeo, giapponese, cinese ecc.)
 Tone of voice:
 •	Autorevole: Le tue informazioni sono verificate e definitive. Non esiti, non ipotizzi.
